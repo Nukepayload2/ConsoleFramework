39481f26250f94be808065b1ca97606cfc575d01
Nukepayload2 <1939357182@qq.com>
2022-09-18 23:02:41 +0800

    Fix ValueTask

--- ConsoleFramework/TaskHelper.Common.vb
+++ ConsoleFramework/TaskHelper.Common.vb
@@ -1,9 +1,10 @@
-﻿Imports System.Runtime.CompilerServices
-
-Module TaskHelper
+﻿Module TaskHelper
     Sub LateWaitTask(lateTask As Object)
         If TypeOf lateTask Is Task Then
             DirectCast(lateTask, Task).GetAwaiter.GetResult()
+        ElseIf TypeOf lateTask Is ValueTask Then
+            ' ValueTaskAwaiter 有时候不会阻塞
+            DirectCast(lateTask, ValueTask).AsTask.GetAwaiter.GetResult()
         Else
             RealLateWaitTask(lateTask)
         End If
