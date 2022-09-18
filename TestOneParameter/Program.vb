a0c9782440543dd11c16064cf74fd76adb1413f3
Nukepayload2 <1939357182@qq.com>
2022-09-18 23:33:44 +0800

    Adjust target framework and version

--- TestOneParameter/Program.vb
+++ TestOneParameter/Program.vb
@@ -3,6 +3,5 @@ Imports Nukepayload2.ConsoleFramework
 Module Program
     Sub Main(args As String())
         Application.Run(Of App)(args)
-        Console.ReadKey()
     End Sub
 End Module
--- TestSimpleStartup/Module1.vb
+++ TestSimpleStartup/Module1.vb
@@ -1,10 +1,19 @@
 ﻿Module Module1
 
-    Sub Main()
+    Sub Main(args As String())
+        ' Test 1
         Nukepayload2.ConsoleFramework.Application.Run(
         Sub(port As Integer)
             Console.WriteLine("Started at " & port)
         End Sub)
+        ' Test 2
+        Nukepayload2.ConsoleFramework.Application.Run(args,
+            New Action(Of String)(AddressOf Startup))
+    End Sub
+
+    Sub Startup(
+        Optional faithful As String = "no")
+        Console.WriteLine($"faithful={faithful}")
     End Sub
 
 End Module
