b64d2cfbe4d3b3218b55912cbe47a6a32afe5796
Nukepayload2 <1939357182@qq.com>
2022-09-18 23:10:17 +0800

    Refactor TaskHelper

--- ConsoleFramework/TaskHelper.LateBindingApproach.vb
+++
@@ -1,6 +0,0 @@
-﻿Partial Module TaskHelper
-    ' .NET Framework, .NET Core 3.0+
-    Private Sub RealLateWaitTask(lateTask As Object)
-        lateTask.GetAwaiter().GetResult()
-    End Sub
-End Module
--- ConsoleFramework/TaskHelper.vb
+++ ConsoleFramework/TaskHelper.vb
@@ -1,7 +1,17 @@
 ﻿Imports System.Reflection
 
-Partial Module TaskHelper
-    ' .NET Standard 2.0
+Module TaskHelper
+    Sub LateWaitTask(lateTask As Object)
+        If TypeOf lateTask Is Task Then
+            DirectCast(lateTask, Task).GetAwaiter.GetResult()
+        ElseIf TypeOf lateTask Is ValueTask Then
+            ' ValueTaskAwaiter 有时候不会阻塞
+            DirectCast(lateTask, ValueTask).AsTask.GetAwaiter.GetResult()
+        Else
+            RealLateWaitTask(lateTask)
+        End If
+    End Sub
+
     Private Sub RealLateWaitTask(lateTask As Object)
         Dim tskTp = lateTask.GetType
         Dim getAwaiter = tskTp.GetRuntimeMethod("GetAwaiter", Type.EmptyTypes)
