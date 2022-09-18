014787aa856c45e34a2ade4bbd8ed5b1758235d9
Nukepayload2 <1939357182@qq.com>
2022-09-18 23:02:53 +0800

    Test ValueTask

---
+++ TestAsyncConsoleApp_ValueTask/Program.cs
@@ -0,0 +1,11 @@
+﻿Nukepayload2.ConsoleFramework.Application.Run(
+async ValueTask (int port) =>
+{
+    Console.WriteLine(3);
+    await Task.Delay(1000);
+    Console.WriteLine(2);
+    await Task.Delay(1000);
+    Console.WriteLine(1);
+    await Task.Delay(1000);
+    Console.WriteLine("Started at " + port);
+});
